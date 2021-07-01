6452004d09d09199ff7f015f47d84b6ef8cc013c
Thiago Monteiro de Paula <thiago.paula@luizalabs.com>
2021-07-01 19:46:04 -0300

    docs: adds swagger

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,9 +1,13 @@
 import {Router} from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+
 import userRouter from '../routes/user/user.routes';
 import tagRouter from '../routes/tag/tag.routes';
 import complimentRouter from '../routes/compliments/compliment.routes';
 
 const router = Router();
+const swaggerDocument = yaml.load('src/docs/swagger.yml');
 
 router.get('/ping', (req, res) => {
   return res.json('pong');
@@ -12,5 +16,7 @@ router.get('/ping', (req, res) => {
 router.use('/user', userRouter);
 router.use('/tag', tagRouter);
 router.use('/compliment', complimentRouter);
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerDocument));
 
 export default router;
